a7270726180568584de1c50cdf1eefe9937bbb17
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-04-08 12:43:01 +0000

    product schema

---
+++ src/models/product.schema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: ["true", "please provide a product name"],
+        trim: true,
+        maxLength: [120, "product name should not be max than 120 chars"]
+    },
+    price: {
+        type: Number,
+        required: ["true", "please provide a product price"],
+        maxLength: [5, "product name should not be max than 5 chars"]
+    },
+    description: {
+        type: String
+    },
+    photos: [
+        {
+            secure_url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        default: 0
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    collectionId: {
+        ref: "Collection"
+    }
+}, {timestamps: true})
+
+
+export default mongoose.model("Product", productSchema)
