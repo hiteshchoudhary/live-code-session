63adb09cbeffc22b38f31e506cba66732002b249
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-04-08 12:06:39 +0000

    forgot password functionality added

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,9 @@ dotenv.config()
 
 const config = {
     PORT: process.env.PORT || 5000,
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm"
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm",
+    JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d"
 }
 
 export default config
