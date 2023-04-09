359f79f1efac3681f95ede5742afd5a6da43c670
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-04-09 11:35:33 +0000

    user logged in and out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,6 +8,14 @@ export const cookieOptions = {
     httpOnly: true
 }
 
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @returns User Object
+ ******************************************************/
+
+
 export const signUp = asyncHandler(async(req, res) => {
     //get data from user
     const {name, email, password } = req.body
@@ -48,4 +56,47 @@ export const signUp = asyncHandler(async(req, res) => {
     })
 
 
+})
+
+
+export const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    //validation
+    if (!email || !password) {
+        throw new CustomError("PLease fill all details", 400)
+    }
+
+    const user = User.findOne({email}).select("+password")
+
+    if (!user) {
+        throw new CustomError("Invalid credentials", 400)
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if (isPasswordMatched) {
+        const token = user.getJWTtoken()
+        user.password = undefined
+        res.cookie("token", token, cookieOptions)
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+
+    throw new CustomError("Password is incorrect", 400)
+})
+
+export const logout = asyncHandler(async (req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Logged Out'
+    })
 })
