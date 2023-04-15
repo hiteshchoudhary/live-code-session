9e7759a8f404e8fb8d46389e65c9dba43394f51c
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-04-15 11:10:09 +0000

    uploaded image to s3 aws

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,11 @@ const config = {
     PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm",
     JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d"
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    S3_REGION: process.env.S3_REGION,
 }
 
 export default config
---
+++ src/service/imageUpload.js
@@ -0,0 +1,21 @@
+import s3 from "../config/s3.config.js"
+
+export const s3FileUpload = async ({bucketName, key, body, contentType}) => {
+    return await s3.upload({
+        Bucket: bucketName,
+        Key: key,
+        Body: body,
+        ContentType: contentType
+    })
+    .promise()
+}
+
+export const s3deleteFile = async ({bucketName, key}) => {
+    return await s3.deleteObject({
+        Bucket: bucketName,
+        Key: key,
+        
+    })
+    .promise()
+}
+
